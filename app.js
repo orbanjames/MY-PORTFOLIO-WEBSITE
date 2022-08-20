9e19b743b01b9506e5b52b1cd2585e54631068f3
orbanjames <96356809+orbanjames@users.noreply.github.com>
2022-08-20 02:56:02 +0200

    subsequent commits and updates

--- app.js
+++ app.js
@@ -1,38 +1,13 @@
-const sections = document.querySelectorAll('.section')
-const sectBtns = document.querySelectorAll('.controls')
-const sectBtn = document.querySelectorAll('.control')
-const allSections = document.querySelector('.main-content')
-function pageTransitions() {
-  //button click active class
-  for (let i = 0; i < sectBtn.length; i++) {
-    sectBtn[i].addEventListener('click', function () {
-      let currentBtn = document.querySelectorAll('.active-btn')
-      currentBtn[0].className = currentBtn[0].className.replace(
-        'active-btn',
-        ''
-      )
-      this.className += 'active-btn'
+;(function () {
+  ;[...document.querySelectorAll('.control')].forEach((button) => {
+    button.addEventListener('click', function () {
+      document.querySelector('.active-btn').classList.remove('active-btn')
+      this.classList.add('active-btn')
+      document.querySelector('.active').classList.remove('active')
+      document.getElementById(button.dataset.id).classList.add('active')
     })
-  }
-
-  // section active class
-  allSections.addEventListener('click', (e) => {
-    const id = e.target.dataset.id
-    if (id) {
-      // remove selected from the btns
-      sectBtns.forEach((btn) => {
-        btn.classList.remove('active')
-      })
-      e.target.classList.add('active')
-
-      //hide other sections
-      sections.forEach((section) => {
-        section.classList.remove('active')
-      })
-      const element = document.getElementById(id)
-      element.classList.add('active')
-    }
   })
-}
-
-pageTransitions()
+  document.querySelector('.theme-btn').addEventListener('click', () => {
+    document.body.classList.toggle('light-mode')
+  })
+})()
